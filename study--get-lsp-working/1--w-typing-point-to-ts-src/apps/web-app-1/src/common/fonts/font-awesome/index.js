4dc1df85c2645b5d88ad348d8e262b912fde9840
Apollo Tang <tang.apollo@gmail.com>
2021-08-02 13:13:25 -0400

    rename folder

--- study--get-lsp-working/1--w-typing-point-to-ts-src/apps/web-app-1/src/common/fonts/font-awesome/index.js
+++
@@ -1,6 +0,0 @@
-const thisNeverRun = () => {
-  // This never run, but to trick webpack into loading font
-  const pathToFont = './fonts/';
-  require(`${pathToFont}`);
-}
-require('./font-awesome.css');
