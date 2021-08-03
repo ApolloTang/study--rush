7c60184d00db29ccdcf0a497665b5ff5c2489be4
Apollo Tang <tang.apollo@gmail.com>
2021-08-02 23:50:24 -0400

    [project-reference] fix type-validation

--- 01-study--get-lsp-working/2--w-project-reference/apps/web-app-1/src/app/index.tsx
+++ 01-study--get-lsp-working/2--w-project-reference/apps/web-app-1/src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import img from './react.png';
-import MyButton from '@mylibs/ui--button';
+// import MyButton from '@mylibs/ui--button';
 import s from './style.module.less';
 import Component1 from '@mylibs/ui--component-1';
 
@@ -9,7 +9,7 @@ import FontTest from '../font-test/';
 const App = () => (
   <div className={`${s.app}`}>
     <h1>Hello React</h1>
-    <div><MyButton>mybutton</MyButton> </div>
+    {/* <div><MyButton>mybutton</MyButton> </div> */}
     <div className={`${s.imageContainer}`}><img src={img} /></div>
     <Component1 />
     <FontTest />
