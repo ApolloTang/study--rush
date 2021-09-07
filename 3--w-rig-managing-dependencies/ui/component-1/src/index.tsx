22657e5ad103e752d530b3ca92405ae690470583
Apollo Tang <tang.apollo@gmail.com>
2021-09-07 12:31:11 -0400

    [3] workding:: wed-app-1:dev,type-validate

--- 3--w-rig-managing-dependencies/ui/component-1/src/index.tsx
+++ 3--w-rig-managing-dependencies/ui/component-1/src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import Cat from '@mylibs/ui--react-cat-image';
+import React from "react";
+import styled from "styled-components";
+// import Cat from '@mylibs/ui--react-cat-image';
 
 const Component1 = styled.div`
   border: 1px solid blue;
   padding: 10px;
-`
+`;
 
-const Button = () => <Component1>
-   <Cat />
- </Component1>
+const Button = () => (
+  <Component1>
+    <div> this is component-1 </div>
+    {/* <Cat /> */}
+  </Component1>
+);
 
-export default Button
-
+export default Button;
