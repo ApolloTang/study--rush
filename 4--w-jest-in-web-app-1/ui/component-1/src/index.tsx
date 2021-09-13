9b733a8ef759f03abe96c541087fea83ad06e2e2
Apollo Tang <tang.apollo@gmail.com>
2021-09-13 00:13:41 -0400

    [4] add an images for component-1

--- 4--w-jest-in-web-app-1/ui/component-1/src/index.tsx
+++ 4--w-jest-in-web-app-1/ui/component-1/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import img from './peanuts/peanuts.jpg'
+
 // import Cat from '@myscope/ui--react-cat-image';
 
 const ComponentStyled = styled.div`
@@ -10,6 +12,10 @@ const ComponentStyled = styled.div`
 const Component = () => (
   <ComponentStyled>
     <div>This is @myscope/ui-component-1 with styled</div>
+    <div>here is an images of peanutsx</div>
+    <div>
+      <img src={img} width="150px"/>
+    </div>
     {/* <Cat /> */}
   </ComponentStyled>
 );
